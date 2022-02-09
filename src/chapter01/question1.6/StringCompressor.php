b1f3960e687f590dbc27eb01e69c7f8fa9c17ecf
Bear <bear.hsiung@drsays.org>
2022-02-09 17:39:22 +0800

    chapter01 question1.6

--- src/chapter01/question1.6/StringCompressor.php
+++ src/chapter01/question1.6/StringCompressor.php
@@ -1,36 +1,31 @@
 <?php
 
-class StringCompressor {
-    public static function compress($original) {
-        $originalLength = strlen($original);
-        $compressedTokens = [];
-        $compressedLength = 0;
-        $lastChar = null;
-        $sameCharCount = 0;
-        for ($i=0; $i<$originalLength; $i++) {
-            $char = $original[$i];
-            if ($lastChar !== null && $char !== $lastChar) {
-                $compressedToken = $lastChar . $sameCharCount;
-                $compressedLength += strlen($compressedToken);
-                if ($compressedLength >= $originalLength) {
-                    return $original;
-                }
-                $compressedTokens[] = $compressedToken;
-                $sameCharCount = 1;
-            } else {
-                $sameCharCount++;
+class StringCompressor
+{
+    public static function compress(string $original): string
+    {
+        $translated = "";
+
+        $count = 1;
+
+        for ($i = 0; $i < strlen($original); $i++) {
+            //last character
+            if (($i + 1) == strlen($original)) {
+                $translated = $translated . $original[$i] . $count;
+                break;
             }
-            $lastChar = $char;
-        }
-        // if the buffer isn't empty, flush it
-        if ($sameCharCount > 0) {
-            $compressedToken = $lastChar . $sameCharCount;
-            $compressedLength += strlen($compressedToken);
-            if ($compressedLength >= $originalLength) {
-                return $original;
+
+            if ($original[$i] == $original[$i + 1]) {
+                $count++;
+                continue;
             }
-            $compressedTokens[] = $compressedToken;
+
+            $translated = $translated . $original[$i] . $count;
+            $count = 1;
         }
-        return implode($compressedTokens);
+
+        return (strlen($original) <= strlen($translated))
+            ? $original
+            : $translated;
     }
 }
