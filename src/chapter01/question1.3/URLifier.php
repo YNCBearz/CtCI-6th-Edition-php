6e1cd850f8b851ba62b26657743247f7687a9a80
YNCBearz <bear19930414@gmail.com>
2022-02-09 20:46:44 +0800

    chapter01 question1.4

--- src/chapter01/question1.3/URLifier.php
+++ src/chapter01/question1.3/URLifier.php
@@ -1,27 +1,11 @@
 <?php
 
-class URLifier {
-    public static function urlify(&$str) {
-        // start at the end and swallow all spaces
-        $length = strlen($str);
-        for ($i = $length - 1; $i >= 0; $i--) {
-            if ($str[$i] !== ' ') {
-                break;
-            }
-        }
-        // fill in string in reverse order
-        // begining with the first non-space
-        // character from the right
-        $j = $length - 1;
-        while ($i >= 0) {
-            if ($str[$i] === ' ') {
-                $str[$j--] = '0';
-                $str[$j--] = '2';
-                $str[$j--] = '%';
-            } else {
-                $str[$j--] = $str[$i];
-            }
-            $i--;
-        }
+class URLifier
+{
+    public static function urlify(&$str)
+    {
+        $str = trim($str);
+
+        $str = str_replace(' ', '%20', $str);
     }
 }
