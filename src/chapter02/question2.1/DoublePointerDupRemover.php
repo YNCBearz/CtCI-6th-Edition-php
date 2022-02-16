26145d277457b1b3e0d72c6680a4e44a59c3c225
YNCBearz <bear19930414@gmail.com>
2022-02-16 20:29:52 +0800

    question 2.2

--- src/chapter02/question2.1/DoublePointerDupRemover.php
+++ src/chapter02/question2.1/DoublePointerDupRemover.php
@@ -1,24 +1,30 @@
 <?php
+
 require_once __DIR__ . '/../../lib/Node.php';
 
 class DoublePointerDupRemover
 {
     public static function removeDups(Node $node)
     {
+        self::removeDuplicated($node);
+    }
+
+    public static function removeDuplicated(Node $node)
+    {
         $pointer1 = $node;
 
         while (!is_null($pointer1)) {
             $pointer2 = $pointer1;
 
-            while (!is_null($pointer2->getNext())) {
-                if ($pointer2->getNext()->getData() == $pointer1->getData()) {
-                    $pointer2->setNext($pointer2->getNext()->getNext());
+            while (!is_null($pointer2->next)) {
+                if ($pointer2->next->data == $pointer1->data) {
+                    $pointer2->next = $pointer2->next->next;
                 } else {
-                    $pointer2 = $pointer2->getNext();
+                    $pointer2 = $pointer2->next;
                 }
             }
 
-            $pointer1 = $pointer1->getNext();
+            $pointer1 = $pointer1->next;
         }
     }
 }
