20b5c7a2e92358b33f406065c7919ec5b90eae33
Bear <bear.hsiung@drsays.org>
2022-02-16 10:23:02 +0800

    question 2.1

--- src/chapter02/question2.1/HashTableDupRemover.php
+++ src/chapter02/question2.1/HashTableDupRemover.php
@@ -1,21 +1,25 @@
 <?php
 require_once __DIR__ . '/../../lib/Node.php';
 
-class HashTableDupRemover {
-    public static function removeDups(Node $node) {
+class HashTableDupRemover
+{
+    public static function removeDups(Node $node)
+    {
         $hashTable = [];
-        $previousNode = $node;
-        $node = $node !== null ? $node->getNext() : null;
-        while ($node !== null) {
-            $data = $node->getData();
-            if (array_key_exists($data, $hashTable)) {
-                // remove the current node from the linked list
+
+        /**
+         * @var ?Node $previousNode
+         */
+        $previousNode = null;
+
+        while (!is_null($node)) {
+            if (in_array($node->getData(), $hashTable)) {
                 $previousNode->setNext($node->getNext());
-                $node = $previousNode;
             } else {
-                $hashTable[$data] = $node;
+                $hashTable[] = $node->getData();
                 $previousNode = $node;
             }
+
             $node = $node->getNext();
         }
     }
