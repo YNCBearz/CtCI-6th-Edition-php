4f9edd764a7f8a94e36662b8eb093b616aab50e0
Bear <bear.hsiung@drsays.org>
2022-02-18 11:29:10 +0800

    question 2.3

--- src/chapter02/question2.3/MiddleNodeDeleter.php
+++ src/chapter02/question2.3/MiddleNodeDeleter.php
@@ -1,17 +1,15 @@
 <?php
 require_once __DIR__ . '/../../lib/Node.php';
 
-class MiddleNodeDeleter {
-    public static function deleteNodeFromLinkedList(Node $node) {
-        if ($node === null) {
-            throw new InvalidArgumentException('node is null');
+class MiddleNodeDeleter
+{
+    public static function deleteNodeFromLinkedList(Node $node)
+    {
+        if (is_null($node) || is_null($node->next)) {
+            return false;
         }
-        $next = $node->getNext();
-        if ($next === null) {
-            throw new InvalidArgumentException('node is not in the middle of a linked list');
-        }
-        // overwrite values in the deleted node with the ones from the next node.
-        $node->setData($next->getData());
-        $node->setNext($next->getNext());
+
+        $node->data = $node->next->data;
+        $node->next = $node->next->next;
     }
 }
