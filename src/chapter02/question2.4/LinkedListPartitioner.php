0f8ea15f23896fc8eb5da517928da3d05917790d
Bear <bear.hsiung@drsays.org>
2022-02-18 12:18:22 +0800

    question 2.4

--- src/chapter02/question2.4/LinkedListPartitioner.php
+++ src/chapter02/question2.4/LinkedListPartitioner.php
@@ -1,28 +1,32 @@
 <?php
 require_once __DIR__ . '/../../lib/Node.php';
 
-class LinkedListPartitioner {
-    public static function partition(Node $node, $x) {
-        // build 2 linked lists: one for the values less than $x
-        // and one for the values greater than or equal to $x
-        $lowerHead = new Node(0); // placeholder node at the head
-        $lowerTail = $lowerHead;
-        $upperHead = new Node(0); // placeholder node at the head
-        $upperTail = $upperHead;
-        while ($node !== null) {
-            if ($node->getData() < $x) {
-                $lowerTail->setNext($node);
-                $lowerTail = $node;
+class LinkedListPartitioner
+{
+    public static function partition(Node $node, $x)
+    {
+        $head = new Node();
+        $headStart = $head;
+
+        $tail = new Node();
+        $tailStart = $tail;
+
+        while (!is_null($node)) {
+            $data = $node->data;
+
+            if ($data < $x) {
+                $head->next = new Node($data);
+                $head = $head->next;
             } else {
-                $upperTail->setNext($node);
-                $upperTail = $node;
+                $tail->next = new Node($data);
+                $tail = $tail->next;
             }
-            $node = $node->getNext();
+
+            $node = $node->next;
         }
-        // attach the upper list to the lower list
-        // and discard the placeholder nodes
-        $lowerTail->setNext($upperHead->getNext());
-        $upperTail->setNext(null);
-        return $lowerHead->getNext();
+
+        $head->next = $tailStart->next;
+
+        return $headStart->next;
     }
 }
