e226e5768d8992e8bed71b1d63ffb074504d27b3
YNCBearz <bear19930414@gmail.com>
2022-02-19 15:27:13 +0800

    update

--- src/chapter02/question2.3/MiddleNodeDeleter.php
+++ src/chapter02/question2.3/MiddleNodeDeleter.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../../lib/Node.php';
 
 class MiddleNodeDeleter
 {
-    public static function deleteNodeFromLinkedList(Node $node)
+    public static function deleteNodeFromLinkedList(?Node $node)
     {
         if (is_null($node) || is_null($node->next)) {
             return false;
