2e15a6155e2b53205806e2cd4d61eadf993d9684
YNCBearz <bear19930414@gmail.com>
2022-02-20 23:32:11 +0800

    question 2.6

--- src/chapter02/question2.6/LinkedListPalindromeChecker.php
+++ src/chapter02/question2.6/LinkedListPalindromeChecker.php
@@ -1,16 +1,41 @@
 <?php
+
 require_once __DIR__ . '/../../lib/LinkedList.php';
 
-class LinkedListPalindromeChecker {
-    public static function isPalindrome(LinkedList $list) {
-        if ($list->getSize() < 2) {
-            return true;
+class LinkedListPalindromeChecker
+{
+    public static function isPalindrome(?Node $node): bool
+    {
+        $reversed = self::reverseAndClone($node);
+        return self::isEqual($node, $reversed);
+    }
+
+    private static function reverseAndClone(Node $node): ?Node
+    {
+        $head = null;
+
+        while (!is_null($node)) {
+            $n = new Node($node->data);
+            $n->next = $head;
+
+            $head = $n;
+            $node = $node->next;
         }
-        $first = $list->removeFirst();
-        $last = $list->removeLast();
-        if ($first !== $last) {
-            return false;
+
+        return $head;
+    }
+
+    private static function isEqual(?Node $node, ?Node $reversed): bool
+    {
+        while (!is_null($node) && !is_null($reversed)) {
+            if ($node->data != $reversed->data) {
+                return false;
+            }
+
+            $node = $node->next;
+            $reversed = $reversed->next;
         }
-        return self::isPalindrome($list);
+
+        return $node === $reversed;
     }
 }
--- test/chapter02/question2.6/LinkedListPalindromeCheckerTest.php
+++ test/chapter02/question2.6/LinkedListPalindromeCheckerTest.php
@@ -1,27 +1,40 @@
 <?php
+
 $baseDir = __DIR__ . '/../../../src/chapter02/question2.6';
 require_once $baseDir . '/LinkedListPalindromeChecker.php';
 
-class LinkedListPalindromeCheckerTest extends \PHPUnit\Framework\TestCase {
+class LinkedListPalindromeCheckerTest extends \PHPUnit\Framework\TestCase
+{
 
-    public function testIsPalindrome() {
-        $list = new LinkedList();
-        $list->add("A");
-        $list->add("B");
-        $list->add("C");
-        $list->add("D");
-        $list->add("C");
-        $list->add("B");
-        $list->add("A");
-        $this->assertTrue(LinkedListPalindromeChecker::isPalindrome($list));
+    public function testIsPalindrome()
+    {
+        $node1 = new Node('A');
+        $head = $node1;
+
+        $node2 = new Node('B');
+        $node3 = new Node('C');
+        $node4 = new Node('B');
+        $node5 = new Node('A');
+
+        $node1->next = $node2;
+        $node2->next = $node3;
+        $node3->next = $node4;
+        $node4->next = $node5;
+
+        $this->assertTrue(LinkedListPalindromeChecker::isPalindrome($head));
     }
 
-    public function testIsNotPalindrome() {
-        $list = new LinkedList();
-        $list->add("A");
-        $list->add("B");
-        $list->add("C");
-        $list->add("D");
-        $this->assertFalse(LinkedListPalindromeChecker::isPalindrome($list));
+    public function testIsNotPalindrome()
+    {
+        $node1 = new Node('A');
+        $head = $node1;
+
+        $node2 = new Node('B');
+        $node3 = new Node('C');
+
+        $node1->next = $node2;
+        $node2->next = $node3;
+
+        $this->assertFalse(LinkedListPalindromeChecker::isPalindrome($head));
     }
 }
