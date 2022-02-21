40d254592ba6e0b0cf028b6746247c47fb4d8bdc
YNCBearz <bear19930414@gmail.com>
2022-02-21 23:35:27 +0800

    Stack

---
+++ src/lib/Stack1.php
@@ -0,0 +1,53 @@
+<?php
+
+class Stack
+{
+    protected ?StackNode $top;
+
+    public function pop()
+    {
+        if (is_null($this->top)) {
+            throw new Exception('Empty Stack');
+        }
+
+        $item = $this->top->data;
+        $this->top = $this->top->next;
+
+        return $item;
+    }
+
+    public function push($item)
+    {
+        $stackNode = new StackNode($item);
+        $stackNode->next = $this->top;
+
+        $this->top = $stackNode;
+    }
+
+    public function peek()
+    {
+        if (is_null($this->top)) {
+            throw new Exception('Empty Stack');
+        }
+
+        return $this->top->data;
+    }
+
+    public function isEmpty()
+    {
+        return $this->top == null;
+    }
+
+}
+
+class StackNode
+{
+    public mixed $data;
+    public ?StackNode $next;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+        $this->next = null;
+    }
+}
