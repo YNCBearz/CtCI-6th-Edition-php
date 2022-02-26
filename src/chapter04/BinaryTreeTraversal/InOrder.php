5e00697c15d23525e14416e5858f2069e618ff38
YNCBearz <bear19930414@gmail.com>
2022-02-27 00:24:52 +0800

    InOrder

--- src/chapter04/BinaryTreeTraversal/InOrder.php
+++ src/chapter04/BinaryTreeTraversal/InOrder.php
@@ -1,1 +1,27 @@
 <?php
+
+class InOrder
+{
+    protected string $result = '';
+
+    public function run(BinaryTreeNode $node): string
+    {
+        $this->InOrderTraversal($node);
+
+        return $this->result;
+    }
+
+    public function InOrderTraversal(?BinaryTreeNode $node)
+    {
+        if ($node != null) {
+            $this->InOrderTraversal($node->left);
+            $this->visit($node);
+            $this->InOrderTraversal($node->right);
+        }
+    }
+
+    private function visit(BinaryTreeNode $node)
+    {
+        $this->result = $this->result . $node->data;
+    }
+}
