7cb24f40bcc3ae9cb19378a5b1633fb5297a5410
YNCBearz <bear19930414@gmail.com>
2022-03-01 22:14:42 +0800

    4.04 first answer

--- src/chapter04/question4.04/BalancedTreeChecker.php
+++ src/chapter04/question4.04/BalancedTreeChecker.php
@@ -1,26 +1,31 @@
 <?php
+
 require_once __DIR__ . '/../../lib/BinaryTreeNode.php';
 
-class BalancedTreeChecker {
-    public static function isBalanced(BinaryTreeNode $node) {
-        return self::getMaxDepthOrNegativeOneIfUnbalanced($node) != -1;
+class BalancedTreeChecker
+{
+    public static function isBalanced(?BinaryTreeNode $node): bool
+    {
+        if (is_null($node)) {
+            return true;
+        }
+
+        $heightDiff = abs(self::getHeight($node->left) - self::getHeight($node->right));
+
+        if ($heightDiff > 1) {
+            return false;
+        } else {
+            return self::isBalanced($node->left) && self::isBalanced($node->right);
+        }
     }
 
-    public static function getMaxDepthOrNegativeOneIfUnbalanced(BinaryTreeNode $node=null) {
-        if ($node === null) {
-            return 0;
-        }
-        $leftDepth = self::getMaxDepthOrNegativeOneIfUnbalanced($node->getLeft());
-        if ($leftDepth == -1) {
+    public static function getHeight(?BinaryTreeNode $node): int
+    {
+        if (is_null($node)) {
             return -1;
         }
-        $rightDepth = self::getMaxDepthOrNegativeOneIfUnbalanced($node->getRight());
-        if ($rightDepth == -1) {
-            return -1;
-        }
-        if (abs($leftDepth - $rightDepth) > 1) {
-            return -1;
-        }
-        return max($leftDepth, $rightDepth) + 1;
+
+        return max(self::getHeight($node->left), self::getHeight($node->right)) + 1;
     }
+
 }
