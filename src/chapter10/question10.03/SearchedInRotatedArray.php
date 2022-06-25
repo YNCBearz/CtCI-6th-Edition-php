0319d16710f3f437c3e81b5abb6e7c48dcb2295a
YNCBearz <bear19930414@gmail.com>
2022-06-25 23:42:19 +0800

    add test case

--- src/chapter10/question10.03/SearchedInRotatedArray.php
+++ src/chapter10/question10.03/SearchedInRotatedArray.php
@@ -15,5 +15,61 @@ class SearchedInRotatedArray
 				? 0
 				: self::TARGET_NOT_FOUND;
 		}
+
+		if (count($items) == 2) {
+			return $this->searchRight($items, $target, 0);
+		}
+
+		$midPointInfo = $this->getMidPointInfo($items);
+
+		$midPointIndex = $midPointInfo['index'];
+		$midPoint = $midPointInfo['value'];
+
+		if ($midPoint == $target) {
+			return $midPointIndex;
+		}
+
+		if ($midPoint > $target) {
+			if (($target >= $items[$midPointIndex - 1])) {
+				return $this->searchLeft($items, $target, $midPointIndex);
+			}
+
+			if ($target >= $items[$midPointIndex + 1]) {
+				return $this->searchRight($items, $target, $midPointIndex);
+			}
+		}
+	}
+
+	private function getMidPointInfo(array $items): array
+	{
+		$index = (int)floor((count($items) - 1) / 2);
+		$value = $items[$index];
+
+		return [
+			'index' => $index,
+			'value' => $value
+		];
+	}
+
+	private function searchRight(array $items, int $target, int $startIndex): int
+	{
+		for ($i = $startIndex; $i <= count($items) - 1; $i++) {
+			if ($items[$i] == $target) {
+				return $i;
+			}
+		}
+
+		return self::TARGET_NOT_FOUND;
+	}
+
+	private function searchLeft(array $items, int $target, int $startIndex): int
+	{
+		for ($i = $startIndex; $i >= 0; $i--) {
+			if ($items[$i] == $target) {
+				return $i;
+			}
+		}
+
+		return self::TARGET_NOT_FOUND;
 	}
 }
