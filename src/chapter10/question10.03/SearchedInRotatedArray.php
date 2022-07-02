3350686d777a90ddab85e5f05cc1d512b3b0f3ef
YNCBearz <bear19930414@gmail.com>
2022-07-02 14:59:21 +0800

    fix 10.03

--- src/chapter10/question10.03/SearchedInRotatedArray.php
+++ src/chapter10/question10.03/SearchedInRotatedArray.php
@@ -4,9 +4,10 @@ class SearchedInRotatedArray
 {
 	const ITEM_NOT_FOUND = -1;
 
-	public function search(array $items, int $target): int
+	public function search(array $items, int $target, ?int $left = null, ?int $right = null): int
 	{
 		$originalItemsCount = count($items);
+
 		if ($originalItemsCount == 0) {
 			return self::ITEM_NOT_FOUND;
 		}
@@ -17,65 +18,93 @@ class SearchedInRotatedArray
 				: self::ITEM_NOT_FOUND;
 		}
 
-		if ($originalItemsCount == 2) {
-			return $this->searchRight($items, $target, 0);
+		if (is_null($left)) {
+			$left = 0;
 		}
 
-		$items = array_merge($items, $items);
-
-		$midPointInfo = $this->getMidPointInfo($items);
-
-		$midPointIndex = $midPointInfo['index'];
-		$midPoint = $midPointInfo['value'];
-
-		if ($midPoint == $target) {
-			return $this->fixIndex($midPointIndex, $originalItemsCount);
+		if (is_null($right)) {
+			$right = $originalItemsCount - 1;
 		}
 
-		$index = $this->searchRight($items, $target, $midPointIndex);
+		$mid = (int)floor(($left + $right) / 2);
 
-		return $this->fixIndex($index, $originalItemsCount);
+		if ($items[$mid] == $target) {
+			return $mid;
+		}
+
+		if ($items[$left] == $target) {
+			return $left;
+		}
+
+		if ($items[$right] == $target) {
+			return $right;
+		}
+
+		$isLeftOrdered = ($items[$left] <= $items[$mid])
+			? true
+			: false;
+
+		$isRightOrdered = ($items[$mid] <= $items[$right])
+			? true
+			: false;
+
+		if (
+			($items[$left] < $target) &&
+			($target < $items[$mid]) &&
+			$isLeftOrdered
+		) {
+			return $this->binarySearch($items, $target, $left, $mid);
+		}
+
+		if (
+			($items[$mid] < $target) &&
+			($target < $items[$right]) &&
+			$isRightOrdered
+		) {
+			return $this->binarySearch($items, $target, $mid, $right);
+		}
+
+		if ($isLeftOrdered) {
+			return $this->binarySearch($items, $target, $mid, $right);
+		}
+
+		if ($isRightOrdered) {
+			return $this->binarySearch($items, $target, $left, $mid);
+		}
 	}
 
-	private function getMidPointInfo(array $items): array
+	private function binarySearch(array $items, int $target, int $left, int $right): int
 	{
-		$index = (int)floor((count($items) - 1) / 2);
-		$value = $items[$index];
+		$originalItemsCount = count($items);
 
-		return [
-			'index' => $index,
-			'value' => $value
-		];
-	}
-
-	private function searchRight(array $items, int $target, int $startIndex): int
-	{
-		for ($i = $startIndex; $i <= count($items) - 1; $i++) {
-			if ($items[$i] == $target) {
-				return $i;
-			}
+		if ($originalItemsCount == 0) {
+			return self::ITEM_NOT_FOUND;
 		}
 
-		return self::ITEM_NOT_FOUND;
-	}
-
-	private function searchLeft(array $items, int $target, int $startIndex): int
-	{
-		for ($i = $startIndex; $i >= 0; $i--) {
-			if ($items[$i] == $target) {
-				return $i;
-			}
+		if ($originalItemsCount == 1) {
+			return ($items[0] == $target)
+				? 0
+				: self::ITEM_NOT_FOUND;
 		}
 
-		return self::ITEM_NOT_FOUND;
-	}
+		$mid = (int)floor(($left + $right) / 2);
 
-	private function fixIndex($index, $originalItemsCount): int
-	{
-		if ($index > $originalItemsCount - 1) {
-			return $index - $originalItemsCount;
+		if ($items[$mid] == $target) {
+			return $mid;
 		}
 
-		return $index;
+		if ($items[$left] == $target) {
+			return $left;
+		}
+
+		if ($items[$right] == $target) {
+			return $right;
+		}
+
+		if ($items[$mid] > $target) {
+			return $this->binarySearch($items, $target, $left, $mid);
+		} else {
+			return $this->binarySearch($items, $target, $mid, $right);
+		}
 	}
 }
